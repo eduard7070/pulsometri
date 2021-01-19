aedbe0b5e3443b460affb7064641533f31c66f50
ed7070 <ed7070707@mail.ru>
2021-01-19 20:41:51 +0300

    закончил секцию с слайдером плюс адаптив

---
+++ src/js/script.js
@@ -0,0 +1,26 @@
+
+$(document).ready(function(){
+    $('.carusel__inner').slick({
+        speed: 1200 ,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../img/left.png" alt="left"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../img/right.png" alt="right"></button>',
+        draggable: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 921,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+              arrows:false,
+              dots:true
+             
+            }
+          },
+        ]
+        
+    })    
+    });
